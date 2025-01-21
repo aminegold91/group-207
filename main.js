eda999af074c0d14e95139debc92c1a258a0dd13
Lamine el kouss <aminegold91@gmail.com>
2025-01-21 02:19:18 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,4 +2,5 @@ let btn = document.querySelector('.btn');
 let menu = document.querySelector('.menu');
 btn.addEventListener('click', ()=>{
     menu.classList.toggle('active');
+    btn.classList.toggle('active');
 });
